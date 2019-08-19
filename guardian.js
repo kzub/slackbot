289c9af42c7849ef20813ab023903bc88f01ab5d
Zubkov Konstantin <konstantin.zubkov@onetwotrip.com>
2019-08-19 16:49:41 +0300

    fix guardian

--- guardian.js
+++ guardian.js
@@ -2,10 +2,6 @@ const slack = require('@slack/rtm-api');
 const fs = require('fs');
 const request = require('request');
 
-const RtmClient = slack.RtmClient;
-const CLIENT_EVENTS = slack.CLIENT_EVENTS;
-const RTM_EVENTS = slack.RTM_EVENTS;
-const MemoryDataStore = slack.MemoryDataStore;
 const token = process.env.SLACK_API_TOKEN;
 const token2 = process.env.SLACK_API_TOKEN_LEGACY;
 const configName = 'guardian.conf';
@@ -20,49 +16,96 @@ rtm.on('connected', () => {
   console.log('RTM client authenticated!', new Date());
 });
 
-const channelsMap = {};
 const config = JSON.parse(fs.readFileSync(configName));
 
-
 //-----------------------------------------------------------
-function getSlackUser(user) {
-  const userobj = rtm.dataStore.getUserById(user);
-  if (!userobj) {
+async function getSlackUser(userId) {
+  const res = await rtm.webClient.users.info({ user: userId });
+  if (!res.ok) {
     return null;
   }
-  return userobj.name;
-}
-//-----------------------------------------------------------
-function getSlackChannel(channelId) {
-  const chanobj = rtm.dataStore.getChannelById(channelId);
-  if (!chanobj) {
-    return null;
-  }
-  return chanobj.name;
+  return res.user.name;
 }
 
-function processAdminMessage(message, msgChannelId) {
+
+//-----------------------------------------------------------
+async function getSlackUserByName(name) {
+  let oneMoreStep = true;
+  let members = [];
+
+  for (let res ; oneMoreStep == true;) {
+    res = await rtm.webClient.users.list({
+      // limit: 500,
+      cursor:  res && res.response_metadata.next_cursor
+    });
+    if (!res.ok) {
+      return null;
+    }
+    members = members.concat(res.members);
+    // console.log(res.members.length, res.response_metadata.next_cursor, oneMoreStep)
+    oneMoreStep = res.response_metadata.next_cursor && res.response_metadata.next_cursor.length > 0;
+  }
+
+  // console.log(`TOTAL USERS: ${members.length}`);
+  const user = members.filter(m => m.name == name).pop();
+  return user && user.id;
+}
+
+
+async function getSlackChannelByName(name) {
+  let oneMoreStep = true;
+  let channels = [];
+  for (let res ; oneMoreStep == true;) {
+    res = await rtm.webClient.channels.list({
+      exclude_archived: true,
+      // limit: 500,
+      cursor:  res && res.response_metadata.next_cursor
+    });
+    if (!res.ok) {
+      return null;
+    }
+    channels = channels.concat(res.channels);
+    // console.log(res.channels.length, res.response_metadata.next_cursor, oneMoreStep)
+    oneMoreStep = res.response_metadata.next_cursor && res.response_metadata.next_cursor > 0;
+  }
+
+  // console.log(`TOTAL CHANNELS: ${channels.length}`);
+  const channel = channels.filter(m => m.name == name).pop();
+  return channel && channel.id;
+}
+
+
+//-----------------------------------------------------------
+async function getSlackChannel(channelId) {
+  const res = await rtm.webClient.conversations.info({ channel: channelId });
+  if (!res.ok) {
+    return null;
+  }
+  return res.channel.name;
+}
+
+
+async function processAdminMessage(message, msgChannelId) {
   const parts = message.split(' ');
   const [cmd, channel, name]  = parts;
 
   if (cmd === 'add' || cmd === 'addId') {
     if (cmd === 'add') {
-      const user = rtm.dataStore.getUserByName(name);
+      const user = await getSlackUserByName(name);
       if (!user) {
         console.log('not found user:', name, user)
         rtm.sendMessage(`failed. unknown user ${name}`, msgChannelId);
         return;
       }
     } else {
-      const user = getSlackUser(name);
+      const user = await getSlackUser(name);
       if (!user) {
         console.log('not found userId:', name, user)
         rtm.sendMessage(`failed. unknown userId ${name}`, msgChannelId);
         return;
       }
-      nameOverride = user;
     }
-    const channelId = rtm.dataStore.getChannelByName(channel);
+    const channelId = await getSlackChannelByName(channel);
     if (!channelId) {
       rtm.sendMessage(`failed. unknown channel ${channel}`, msgChannelId);
       return;
@@ -71,7 +114,7 @@ function processAdminMessage(message, msgChannelId) {
     if (!config.protect[channel]) {
       config.protect[channel] = {};
     }
-    config.protect[channel][nameOverride || name] = true;
+    config.protect[channel][name] = true;
     fs.writeFileSync(configName, JSON.stringify(config));
     rtm.sendMessage('ok', msgChannelId);
   }
@@ -86,13 +129,13 @@ function processAdminMessage(message, msgChannelId) {
     rtm.sendMessage(msg.join('\n'), msgChannelId);
   }
 
-  else if (cmd === 'del') {
-    const user = rtm.dataStore.getUserByName(name);
+  else if (cmd === 'del' || cmd === 'rem') {
+    const user = await getSlackUserByName(name);
     if (!user) {
       rtm.sendMessage(`failed. unknown user ${name}`, msgChannelId);
       return;
     }
-    const channelId = rtm.dataStore.getChannelByName(channel);
+    const channelId = await getSlackChannelByName(channel);
     if (!channelId) {
       rtm.sendMessage(`failed. unknown channel ${channel}`, msgChannelId);
       return;
@@ -107,21 +150,21 @@ function processAdminMessage(message, msgChannelId) {
   }
 }
 
-rtm.on('message', (message) => {
+rtm.on('message', async (message) => {
   try {
     if (!message.user || !message.text) {
       return;
     }
 
-    const slackuser = getSlackUser(message.user);
+    const slackuser = await getSlackUser(message.user);
     if (!slackuser) {
       console.log('error getting slackuser', message);
       return;
     }
-    const channel = getSlackChannel(message.channel);
+    const channel = await getSlackChannel(message.channel);
     if (!channel) {
       if (config.admins[slackuser]) {
-        processAdminMessage(message.text, message.channel);
+        await processAdminMessage(message.text, message.channel);
         return;
       }
       console.log('error getting channel', message);
@@ -139,7 +182,7 @@ rtm.on('message', (message) => {
     url += `&channel=${message.channel}`;
     url += '&as_user=true&pretty=1';
 
-    request(url, (err, data) => {
+    request(url, (err) => {
       if (err) {
         console.log(err);
       }
